31ff4b3b038e611d3ed7976bba7ff63cd2917c8e
SARANGA PANI SARMA <assam.sps@gmail.com>
2019-03-30 22:49:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,24 +11,24 @@ app.get('/', (req, res) => {
 
 res.send('Chat Server is running on port 3000')
 });
-io.on('connection', function(socket)  {
+io.on('connection', (socket) =>  {
         
-        console.log('user connected');
-        var store =socket.handshake.query.name; 
-        console.log(store);
-        console.log(socket.id); 
+        console.log('user connected')
+        var store =socket.handshake.query.name
+        console.log(store)
+        console.log(socket.id)
 
 socket.on('join', function(userNickname) {
 
-        console.log(userNickname +" : has joined the chat "  )
+        console.log(userNickname +" : has joined the chat "  );
         
-        let qList={"question":["Test question 1?", "Test question 2?","Test question 3?", "Test question 4?","Test question 5?", "Test question 6?"]}
-        console.log("question :" +qList)
+        let qList={"question":["Test question 1?", "Test question 2?","Test question 3?", "Test question 4?","Test question 5?", "Test question 6?"]};
+        console.log("question :" +qList);
         
           
         socket.broadcast.emit('userjoinedthechat',userNickname +" : has joined the chat ");
         
-    });
+    })
 
 
 socket.on('messagedetection', (senderNickname,messageContent) => {
@@ -39,33 +39,33 @@ socket.on('messagedetection', (senderNickname,messageContent) => {
         //create a message object 
        let  message = {"message":messageContent, "senderNickname":senderNickname}
           // send the message to the client side  
-       io.emit('message', message );
+       io.emit('message', message )
      
-      });
+      })
      
  socket.on('disconnect', function() {
     console.log( ' user has left ')
-    socket.broadcast.emit("userdisconnect"," user has left ") ;
+    socket.broadcast.emit("userdisconnect"," user has left ") 
 
-}); 
+})
  
  socket.on('playcontrol', function(mediaplaycontrol) {
     console.log( ' Video Play'+mediaplaycontrol)
-    socket.emit("mediacontrol",mediaplaycontrol) ;
+    socket.emit("mediacontrol",mediaplaycontrol) 
 
-});
-
-
+})
 
 
-});
+
+
+})
 
 
 
 
 
 server.listen(server_port, server_ip_address,()=>{
-console.log( "Listening on " + server_ip_address + ", server_port " + server_port  );
+console.log( "Listening on " + server_ip_address + ", server_port " + server_port  )
 
 
-});
+})
